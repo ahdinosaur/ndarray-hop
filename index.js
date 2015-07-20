8fec4d1763f9cea39228aa42e3c2354b565f0371
Michael Williams <dinosaur@riseup.net>
2015-07-21 03:10:09 +1200

    somehow it works

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
-var ops = require('ndarray-ops')
 var coal = require('coalescy')
+var ndarray = require('ndarray')
+var cbuffer = require('CBuffer')
+var getDtype = require('dtype')
 
 module.exports = hop
 
 function hop (opts, cb) {
   opts = coal(opts, {})
 
-  var shape = opts
+  var frame = coal(opts.frame, {})
+  frame = ndarray([], frame.shape, frame.stride, frame.offset)
+
+  var hopSize = opts.hopSize
+  if (hopSize > frame.size) {
+    throw new Error("Hop size must be smaller than frame size")
+  }
+
+  var bufferSize = coal(
+    opts.bufferSize,
+    frame.size * Math.pow(2, 4)
+  )
+
+  var dtypeName = opts.dtype || 'float32'
+  var Dtype = getDtype(dtypeName)
+
+  var buffer = new cbuffer(bufferSize)
+  buffer.fill(0)
 
   var offset = 0
-
-  // create splices
+  var hopper = 0
 
   // receive ndarray
   return function onArray (arr) {
-    // assign ndarray at offset
+    var input = ndarray(
+      arr.data, arr.shape, arr.stride, arr.offset
+    )
+
+    // assign input array at offset
+    for (var i = 0; i < input.size; i++) {
+      buffer.set(offset + i, arr.data[i])
+    }
 
     // increment offset
+    offset += input.size
 
     // emit any splices
-    cb(arr)
+    var data
+    while (
+      (offset > frame.size) &&
+      ((offset - hopper) >= frame.size)
+    ) {
+      data = buffer.slice(hopper, hopper + frame.size)
+
+      cb(ndarray(
+        new Dtype(data),
+        frame.shape,
+        frame.stride,
+        hopper
+      ))
+      hopper += hopSize
+    }
   }
-}
-
-function assign (arr, offset) {
-
 }
---
+++ test.js
@@ -0,0 +1,85 @@
+var test = require('tape')
+var hop = require('./')
+var slice = Array.prototype.slice
+
+test("hop unaligned", function(t) {
+  
+  var expectedFrames = [
+    [0,1,2,3,4,5,6],
+    [3,4,5,6,7,8,9],
+    [6,7,8,9,10,11,12],
+    [9,10,11,12,13,14,15],
+    [12,13,14,15,16,17,18],
+    [15,16,17,18,19,20,21]
+  ]
+
+  t.plan(expectedFrames.length * 2)
+  
+  var frameShape = [7]
+
+  var slicer = hop({
+    frame: { shape: frameShape },
+    hopSize: 3
+  }, function (x) {
+    var f = expectedFrames[0]
+    expectedFrames.shift()
+    t.deepEqual(slice.call(x.data), f)
+    t.deepEqual(x.shape, frameShape)
+  })
+  
+  function pushData(arr) {
+    slicer({
+      data: new Float32Array(arr),
+      shape: [arr.length]
+    })
+  }
+  
+  pushData([0,1,2])
+  pushData([3,4])
+  pushData([5,6,7,8,9,10,11])
+  pushData([12,13])
+  pushData([14,15,16,17,18,19,20,21])
+})
+
+/*
+require("tape")("frame-hop aligned", function(t) {
+  
+  var expectedFrames = [
+    [0,1,2,3],
+    [2,3,4,5],
+    [4,5,6,7],
+    [6,7,8,9],
+    [8,9,10,11],
+    [10,11,12,13],
+    [12,13,14,15],
+    [14,15,16,17],
+    [16,17,18,19],
+    [18,19,20,21]
+  ]
+  
+  t.plan(expectedFrames.length)
+  
+  var slicer = hop({
+    frameSize: 4,
+    hopSize: 2,
+    maxDataSize: 8
+  })
+  
+  slicer.pipe(through.obj(function (x, enc, cb) {
+    var f = expectedFrames[0]
+    expectedFrames.shift()
+    t.same(Array.prototype.slice.call(x, 0), f)
+    cb()
+  }))
+
+  function pushData(arr) {
+    slicer.write(new Float32Array(arr))
+  }
+  
+  pushData([0,1,2])
+  pushData([3,4])
+  pushData([5,6,7,8,9,10,11])
+  pushData([12,13])
+  pushData([14,15,16,17,18,19,20,21])
+})
+*/
